383763477340bd9542f593eec9c433058e80822a
Connor Hamilton Inglis <chinglis@umich.edu>
2019-06-19 16:35:34 -0400

    Updates

---
+++ precache-manifest.c5e0f3348cb14d21cf22bab9429bd23d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b429ecc562c57bc3eb65d723a70187",
+    "url": "/CodeNames/index.html"
+  },
+  {
+    "revision": "46dc05c652665f860264",
+    "url": "/CodeNames/static/css/main.72aa8272.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/CodeNames/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "46dc05c652665f860264",
+    "url": "/CodeNames/static/js/main.80d65913.chunk.js"
+  },
+  {
+    "revision": "72c15b8632a5ae4f2515",
+    "url": "/CodeNames/static/js/runtime~main.11b6010d.js"
+  }
+]);
