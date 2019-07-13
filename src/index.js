ac6063d1b627711bfa3270fb24d1712fc8972b1b
Connor Hamilton Inglis <chinglis@umich.edu>
2019-07-13 00:21:25 -0400

    deployment

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './Static/css/index.css';
 import App from './Components/App.js';
 import * as serviceWorker from './serviceWorker';
 
-const databaseUrl = "http://localhost:5000/api/v1/"
-//const databaseUrl = "https://codenames-back-end-packs.herokuapp.com/api/v1/"
-const socketUrl = "http://localhost:3231"
-//const socketUrl = "https://codenames-back-end-socket.herokuapp.com/"
+//const databaseUrl = "http://localhost:5000/api/v1/"
+const databaseUrl = "https://codenames-back-end-packs.herokuapp.com/api/v1/"
+//const socketUrl = "http://localhost:3231"
+const socketUrl = "https://codenames-back-end-socket.herokuapp.com/"
 ReactDOM.render(<App url={databaseUrl} socketUrl={socketUrl} webPacksPerPage={3} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
